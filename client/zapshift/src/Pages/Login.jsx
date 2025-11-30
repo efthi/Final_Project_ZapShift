e8d1ab1ec9692af9b8829a4df0ef775b406ba1d4
efthaqur <efthaqur@gmail.com>
2025-11-30 17:31:42 +0600

    Update: Login.jsx and Register.jsx

--- client/zapshift/src/Pages/Login.jsx
+++ client/zapshift/src/Pages/Login.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
-    return (
-        <div>
-           <p>Hello User! Please Login</p> 
-        </div>
-    );
+  return (
+    <>
+      <div className="container mx-auto m-10 min-w-10 min-h-10 justify-center items-center border-2 border-b-emerald-500">
+        <p>Hello User! Please Login</p>
+      </div>
+    </>
+  );
 };
 
 export default Login;
--- client/zapshift/src/Pages/Register.jsx
+++ client/zapshift/src/Pages/Register.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Register = () => {
-    return (
-        <div>
-            <p>Hello Visitor! Please Register!!</p>
-        </div>
-    );
+  return (
+    <div className="container mx-auto m-10 min-w-10 min-h-10 justify-center items-center border-2 border-b-emerald-500">
+      <p>Hello Visitor! Please Register!!</p>
+    </div>
+  );
 };
 
 export default Register;
